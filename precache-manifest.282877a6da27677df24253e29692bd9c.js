ba93f8610129cd05af64a70fa70f3ccc8e41e47e
bhimeshchauhan <bhimeshchauhan@gmail.com>
2020-08-09 16:50:16 -0400

    Updates

---
+++ precache-manifest.282877a6da27677df24253e29692bd9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83a64831bd9efe3f8d67eeca573b164f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1753ea6df858d0214a9d",
+    "url": "/static/css/main.6177a810.chunk.css"
+  },
+  {
+    "revision": "992eedf29b9dbae5398a",
+    "url": "/static/js/2.02d6bd9c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.02d6bd9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1753ea6df858d0214a9d",
+    "url": "/static/js/main.845e3708.chunk.js"
+  },
+  {
+    "revision": "b5abe1ee1d547045b6b4",
+    "url": "/static/js/runtime-main.5f568a5e.js"
+  }
+]);
